e1056c1821ee452a11f0672f05129dbfeb4e9715
unknown <rakeshsmusale03@gmail.com>
2021-12-06 13:58:31 +0530

    UC2 for calculate total wage.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,5 +1,6 @@
+//UC1 -Checked Employee is present or not
 //constant variable
-const IS_ABSENT = 0;
+/*const IS_ABSENT = 0;
 let empCheck = Math.floor(Math.random()*10) % 2;
 if (empCheck == IS_ABSENT)
 {
@@ -8,4 +9,26 @@ if (empCheck == IS_ABSENT)
 else 
 {
     console.log("Employee is Present");
+}*/
+
+//UC2 -Calculate Daily Wage
+//constant variable
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10) % 3;
+switch (empCheck)
+{
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
 }
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage : " +empWage);
